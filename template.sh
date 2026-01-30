8af3ff86917461dfe93e86c96f3d0a93481babb0
Iftikhar Hussain <iftikharhussaini8444@gmail.com>
2026-01-30 16:28:53 +0530

    folder structure added

---
+++ template.sh
@@ -0,0 +1,15 @@
+#directory
+mkdir -p src
+mkdir -p research
+
+#creating files
+touch src/__init__.py
+touch src/helper.py
+touch src/promt.py
+touch .env 
+touch setup.py
+touch app.py
+touch research/trails.ipynb
+touch requirement.txt
+
+echo "file created succesfully"
